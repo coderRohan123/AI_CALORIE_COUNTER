15cd5fe84a73c07dd4934b6fd0dbb08a3aa61028
Rohan Mallick <rohanmallick016@gmail.com>
2024-11-08 22:30:44 +0530

    commited Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ with col2:
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
 submit = st.button("Tell me the total calories")
 
